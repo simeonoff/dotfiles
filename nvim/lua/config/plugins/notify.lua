f48cbad91a53c991a88269d13238e858e47844e1
Simeon Simeonoff <sim.simeonoff@gmail.com>
2023-01-02 00:30:07 +0200

    add notification plugin

--- nvim/lua/config/plugins/notify.lua
+++ nvim/lua/config/plugins/notify.lua
@@ -5,23 +5,20 @@ local M = {
 
 M.config = function()
 	local notify = require("notify")
+
 	notify.setup({
-		config = {
-			timeout = 3000,
-			level = vim.log.levels.INFO,
-			fps = 60,
-			---@usage Animation style one of { "fade", "slide", "fade_in_slide_out", "static" }
-			stages = "static",
-			-- Render function for notifications. See notify-render()
-			render = "default",
-			max_height = function()
-				return math.floor(vim.o.lines * 0.75)
-			end,
-			max_width = function()
-				return math.floor(vim.o.columns * 0.75)
-			end,
-		},
+		stages = "static",
+		timeout = 3000,
+		level = vim.log.levels.INFO,
+		max_height = function()
+			return math.floor(vim.o.lines * 0.75)
+		end,
+		max_width = function()
+			return math.floor(vim.o.columns * 0.75)
+		end,
 	})
+
 	vim.notify = notify
 end
+
 return M
