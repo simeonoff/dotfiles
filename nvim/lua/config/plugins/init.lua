0274778c835f83e40728d0d7eb927dfa5ee2a4ee
Simeon Simeonoff <sim.simeonoff@gmail.com>
2023-10-09 13:33:17 +0300

    indentline: fix config

--- nvim/lua/config/plugins/init.lua
+++ nvim/lua/config/plugins/init.lua
@@ -3,12 +3,9 @@ return {
 	{
 		"lukas-reineke/indent-blankline.nvim",
 		event = "BufReadPre",
+        main = "ibl",
 		config = function()
-			require("indent_blankline").setup({
-				-- for example, context is off by default, use this to turn it on
-				show_current_context = false,
-				show_current_context_start = false,
-			})
+			require("ibl").setup()
 		end,
 	},
 	{
